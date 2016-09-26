60d9bda50e2b96cb077d69496f7f35a37e637d53
neal.ma <neal.ma.sh@gmail.com>
2016-09-26 20:53:31 +0800

    component example

--- helloworld/index.android.js
+++ helloworld/index.android.js
@@ -9,6 +9,12 @@ import {
   AppRegistry,
   StyleSheet,
   Text,
+  TextInput,
+  ListView,
+  ScrollView,
+  Image,
+  Navigator,
+  Animated,
   View
 } from 'react-native';
 
@@ -71,4 +77,289 @@ class BlinkApp extends Component {
   }
 }
 
-AppRegistry.registerComponent('helloworld', () => BlinkApp);
+/**
+ * Styles
+ */
+
+class Styles extends Component{
+  render() {
+    return (
+      <View>
+        <Text style={[styles.red, styles.bigblue]}>React Native</Text>
+        <Text style={[styles.bigblue, styles.red]}>React Native</Text>
+      </View>
+    );
+  }
+}
+
+var styles=StyleSheet.create({
+  red: {
+    color: 'red',
+  },
+  bigblue: {
+    color: 'blue',
+    fontSize: 30,
+    fontWeight: 'bold'
+  }
+});
+
+/**
+ * Demitions
+ */
+ class FixDemitionsBasics extends Component {
+
+    render() {
+      return (
+        <View>
+          <View style={{width: 50, height: 50, backgroundColor: 'red'}} />
+          <View style={{width: 100, height: 100, backgroundColor: 'blue'}} />
+          <View style={{width: 150, height: 150, backgroundColor: 'pink'}} />
+        </View>
+      );
+    }
+ }
+
+/**
+ * FlexDemitions
+ */
+class FlexDimensionsBasics extends Component {
+  render() {
+    return (
+      // 试试去掉父View中的`flex: 1`。
+      // 则父View不再具有尺寸，因此子组件也无法再撑开。
+      // 然后再用`height: 300`来代替父View的`flex: 1`试试看？
+      <View style={{flex: 1}}>
+        <View style={{flex: 1, backgroundColor: 'powderblue'}} />
+        <View style={{flex: 2, backgroundColor: 'skyblue'}} />
+        <View style={{flex: 3, backgroundColor: 'steelblue'}} />
+      </View>
+    );
+  }
+}
+
+/**
+ * FlexDirection
+ */
+class FlexDirectionBasics extends Component {
+  render() {
+    return (
+      // 尝试把`flexDirection`改为`column`看看
+      <View style={{flex: 1, flexDirection: 'column'}}>
+        <View style={{width: 50, height: 50, backgroundColor: 'powderblue'}} />
+        <View style={{width: 50, height: 50, backgroundColor: 'skyblue'}} />
+        <View style={{width: 50, height: 50, backgroundColor: 'steelblue'}} />
+      </View>
+    );
+  }
+};
+
+/**
+ * JustifyContent
+ */
+class JustifyContentBasics extends Component {
+  render() {
+    return (
+      // 尝试把`justifyContent`改为`center`看看
+      // 尝试把`flexDirection`改为`row`看看
+      <View style={{
+          flex: 1,
+          flexDirection: 'column',
+          justifyContent: 'space-around',
+        }}>
+        <View style={{width: 50, height: 50, backgroundColor: 'powderblue'}} />
+        <View style={{width: 50, height: 50, backgroundColor: 'skyblue'}} />
+        <View style={{width: 50, height: 50, backgroundColor: 'steelblue'}} />
+      </View>
+    );
+  }
+};
+
+/**
+ * AlignItems
+ */
+class AlignItemsBasics extends Component {
+  render() {
+    return (
+      // 尝试把`alignItems`改为`flex-start`看看
+      // 尝试把`justifyContent`改为`flex-end`看看
+      // 尝试把`flexDirection`改为`row`看看
+      <View style={{
+        flex: 1,
+        flexDirection: 'column',
+        justifyContent: 'center',
+        alignItems: 'center',
+      }}>
+        <View style={{width: 50, height: 50, backgroundColor: 'blue'}} />
+        <View style={{width: 50, height: 50, backgroundColor: 'red'}} />
+        <View style={{width: 50, height: 50, backgroundColor: 'pink'}} />
+      </View>
+    );
+  }
+};
+
+/**
+ * TextInput
+ */
+class PizzaTranslator extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {text: ''};
+  }
+
+  render() {
+    return (
+      <View style={{padding: 10}}>
+        <TextInput
+          style={{height: 40}}
+          placeholder="Type here to translate!"
+          onChangeText={(text) => this.setState({text})}
+        />
+        <Text style={{padding: 10, fontSize: 42}}>
+          {this.state.text.split(' ').map((word) => word && '🍕').join(' ')}
+        </Text>
+      </View>
+    );
+  }
+}
+
+/**
+ * ScrollView
+ */
+class IScrolledDownAndWhatHappenedNextShockedMe extends Component {
+  render() {
+      return(
+        <ScrollView>
+          <Text style={{fontSize:96}}>Scroll me plz</Text>
+          <Image source={require('./img/favicon.png')} />
+          <Image source={require('./img/favicon.png')} />
+          <Image source={require('./img/favicon.png')} />
+          <Image source={require('./img/favicon.png')} />
+          <Image source={require('./img/favicon.png')} />
+          <Image source={require('./img/favicon.png')} />
+          <Image source={require('./img/favicon.png')} />
+          <Image source={require('./img/favicon.png')} />
+          <Text style={{fontSize:96}}>If you like</Text>
+          <Image source={require('./img/favicon.png')} />
+          <Image source={require('./img/favicon.png')} />
+          <Image source={require('./img/favicon.png')} />
+          <Image source={require('./img/favicon.png')} />
+          <Image source={require('./img/favicon.png')} />
+          <Image source={require('./img/favicon.png')} />
+          <Image source={require('./img/favicon.png')} />
+          <Image source={require('./img/favicon.png')} />
+          <Text style={{fontSize:96}}>Scrolling down</Text>
+          <Image source={require('./img/favicon.png')} />
+          <Image source={require('./img/favicon.png')} />
+          <Image source={require('./img/favicon.png')} />
+          <Image source={require('./img/favicon.png')} />
+          <Image source={require('./img/favicon.png')} />
+          <Image source={require('./img/favicon.png')} />
+          <Image source={require('./img/favicon.png')} />
+          <Image source={require('./img/favicon.png')} />
+          <Text style={{fontSize:96}}>What's the best</Text>
+          <Image source={require('./img/favicon.png')} />
+          <Image source={require('./img/favicon.png')} />
+          <Image source={require('./img/favicon.png')} />
+          <Image source={require('./img/favicon.png')} />
+          <Image source={require('./img/favicon.png')} />
+          <Image source={require('./img/favicon.png')} />
+          <Image source={require('./img/favicon.png')} />
+          <Image source={require('./img/favicon.png')} />
+          <Text style={{fontSize:96}}>Framework around?</Text>
+          <Image source={require('./img/favicon.png')} />
+          <Image source={require('./img/favicon.png')} />
+          <Image source={require('./img/favicon.png')} />
+          <Image source={require('./img/favicon.png')} />
+          <Image source={require('./img/favicon.png')} />
+          <Image source={require('./img/favicon.png')} />
+          <Image source={require('./img/favicon.png')} />
+          <Image source={require('./img/favicon.png')} />
+          <Text style={{fontSize:80}}>React Native</Text>
+
+        </ScrollView>
+    );
+  }
+}
+
+/**
+ * ListView
+ */
+class ListViewBasics extends Component {
+  // 初始化模拟数据
+  constructor(props) {
+    super(props);
+    const ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
+    this.state = {
+      dataSource: ds.cloneWithRows([
+        'John', 'Joel', 'James', 'Jimmy', 'Jackson', 'Jillian', 'Julie', 'Devin'
+        , 'Joel', 'James', 'Jimmy', 'Jackson', 'Jillian', 'Julie', 'Devin'
+        , 'Joel', 'James', 'Jimmy', 'Jackson', 'Jillian', 'Julie', 'Devin'
+        , 'Joel', 'James', 'Jimmy', 'Jackson', 'Jillian', 'Julie', 'Devin'
+        , 'Joel', 'James', 'Jimmy', 'Jackson', 'Jillian', 'Julie', 'Devin'
+        , 'Joel', 'James', 'Jimmy', 'Jackson', 'Jillian', 'Julie', 'Devin'
+        , 'Joel', 'James', 'Jimmy', 'Jackson', 'Jillian', 'Julie', 'Devin'
+        , 'Joel', 'James', 'Jimmy', 'Jackson', 'Jillian', 'Julie', 'Devin'
+        , 'Joel', 'James', 'Jimmy', 'Jackson', 'Jillian', 'Julie', 'Devin'
+      ])
+    };
+  }
+  render() {
+    return (
+      <View style={{paddingTop: 22, flex: 1}}>
+        <ListView
+          dataSource={this.state.dataSource}
+          renderRow={(rowData) => <Text>{rowData}</Text>}
+        />
+      </View>
+    );
+  }
+}
+
+/**
+ * Navgitor 
+ */
+import MyScene from './MyScene';
+
+class YoDawgApp extends Component {
+  render() {
+    return (
+      <MyScene title="qiangqiang"/>
+    )
+  }
+}
+
+/**
+ * Animated
+ */
+class Playground extends React.Component {
+  constructor(props: any) {
+    super(props);
+    this.state = {
+      bounceValue: new Animated.Value(0),
+    };
+  }
+  render(): ReactElement {
+    return (
+      <Animated.Image                         // 可选的基本组件类型: Image, Text, View
+        source={{uri: 'http://i.imgur.com/XMKOH81.jpg'}}
+        style={{
+          flex: 1,
+          transform: [                        // `transform`是一个有序数组（动画按顺序执行）
+            {scale: this.state.bounceValue},  // 将`bounceValue`赋值给 `scale`
+          ]
+        }}
+      />
+    );
+  }
+  componentDidMount() {
+    this.state.bounceValue.setValue(1.5);     // 设置一个较大的初始值
+    Animated.spring(                          // 可选的基本动画类型: spring, decay, timing
+      this.state.bounceValue,                 // 将`bounceValue`值动画化
+      {
+        toValue: 0.8,                         // 将其值以动画的形式改到一个较小值
+        friction: 1,                          // Bouncier spring
+      }
+    ).start();                                // 开始执行动画
+  }
+}
+AppRegistry.registerComponent('helloworld', () => Playground);
